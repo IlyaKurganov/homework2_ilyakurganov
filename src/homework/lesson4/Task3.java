61178a00450727513c9bcf446040306e21e9ad67
Ilya Kurganov <iluha.kurganov@yandex.ru>
2024-06-10 10:46:28 +0500

    Initial commit

---
+++ src/homework/lesson4/Task3.java
@@ -0,0 +1,21 @@
+package homework.lesson4;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String inputString = "Sell in may and go away";
+        String str = inputString.toLowerCase();
+        String[] splitedStr = str.split(" ");
+        StringBuilder builder = new StringBuilder();
+
+        for (String s : splitedStr) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String newStr = new String(chars);
+            builder.append(newStr).append(" ");
+        }
+        System.out.println(builder);
+    }
+}
